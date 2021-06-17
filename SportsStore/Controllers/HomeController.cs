f76d3228023b6c7cf1c8e5f2a4d8b352cae40ef6
ChuvashPeople <ChuvashPeople@yandex.ru>
2021-06-17 10:00:40 +0300

    added simple pagination

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,29 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private readonly IStoreRepository _storeRepository;
-
+        int pageSize = 4;
         public HomeController(IStoreRepository storeRepository)
         {
             _storeRepository = storeRepository;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int productPage = 1)
         {
-            return View(_storeRepository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _storeRepository.Products.Count()
+                },
+                Products = _storeRepository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * pageSize)
+                    .Take(pageSize)
+            };
+
+            return View(model);
         }
     }
 }
